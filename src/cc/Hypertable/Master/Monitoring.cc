93a041eedd285f6a826404e7af239637a4dd1439
Willem Joubert <wjoubert@linesjoubert.com>
2015-03-12 13:56:18 +0200

    Removed rddtool command quotes - causes command to fail on some machines

--- src/cc/Hypertable/Master/Monitoring.cc
+++ src/cc/Hypertable/Master/Monitoring.cc
@@ -809,9 +809,9 @@ void Monitoring::run_rrdtool(std::vector<String> &command) {
   String cmd = "env LD_LIBRARY_PATH= DYLD_LIBRARY_PATH= rrdtool";
 
   foreach_ht (const String &s, command) {
-    cmd += " \"";
+    cmd += " ";
     cmd += s;
-    cmd += "\" ";
+    cmd += " ";
   }
 
   HT_DEBUGF("run_rrdtool: %s", cmd.c_str());
